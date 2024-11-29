18306cc4a1f04f353547a4b080edcd20bf543991
Marian <marianxd2345@gmail.com>
2024-11-29 10:57:26 +0200

    second commit

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ using Azure.Identity;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
-
+//
 builder.Services.AddCors(options =>
 {
     options.AddPolicy(name: "CORS",
